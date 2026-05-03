add003202e68727757ea847a41e80c94306135b1
Nelja-creator <nabukenyanajati@gmail.com>
2026-05-03 18:06:04 +0300

    Applying latest security fixes

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
+
 #after installing telegram library in the termianal 
 from telegram import Update
 from telegram.ext import ApplicationBuilder,CommandHandler,ContextTypes
 
-#replace token from botfather
-TOKEN="8774608930:AAH9qZ1fC1eaVQSRXIkLFOfJltqMuqVKbSE"
+# This looks for the .env file and loads the variables
+load_dotenv()
+
+# This grabs your new token safely
+TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 # 'async' means the bot can do other things while waiting for a message to send.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
